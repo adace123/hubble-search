d5d029bcffc7e3791b1de441d38273c6aaf971d9
aaron.feigenbaum <aaron@enervee.com>
2020-10-03 16:22:38 -0700

    update image list component

--- client/src/app/components/hubble-images/hubble-images.component.ts
+++ client/src/app/components/hubble-images/hubble-images.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import HubbleImage from '../../interfaces/hubble-image';
+import { HubbleImagesService } from '../../services/hubble-images/hubble-images.service';
+
 @Component({
   selector: 'app-hubble-images',
   templateUrl: './hubble-images.component.html',
   styleUrls: ['./hubble-images.component.scss']
 })
 export class HubbleImagesComponent implements OnInit {
+  hubbleResources: Array<HubbleImage>;
+  resultCount: number;
+  currentPage: number;
 
-  constructor() { }
+  constructor(private hubbleImageService: HubbleImagesService) { }
 
   ngOnInit() {
+    this.hubbleImageService.getImages().subscribe(data => {
+      this.hubbleResources = data;
+      this.resultCount = data.length;
+    });
+  }
+
+  get numPages(): number {
+    const pages = this.resultCount / 6;
+    
+    if (pages < 1 || pages % 6 === 0) {
+      return pages;
+    }
+    return Math.round(pages) + 1;
+  }
+
+  get currentSlice(): Array<HubbleImage> {
+
+    return null;
   }
 
 }
